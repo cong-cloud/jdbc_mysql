c95ee1d7b7ae9e52799bceeef19cc78edd8c3c57
wang <1223127421@qq.com>
2019-09-10 17:26:46 +0800

    123

--- src/com/UserDao.java
+++ src/com/UserDao.java
@@ -19,25 +19,81 @@ public class UserDao {
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) {
-        List<User> list = select();
-        for (User user : list) {
-            System.out.println(user);
+//        List<User> list = select();
+////        for (User user : list) {
+////            System.out.println(user);
+////        }
+
+        User user = new User(4, "bbb", "111");
+//        save(user);
+//        update(user);
+//        delete(user);
+    }
+
+    public static void delete(User user) {
+        Connection conn = null;
+        PreparedStatement stat = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            String sql = "delete from user where id=?";
+            stat = conn.prepareStatement(sql);
+            stat.setObject(1, user.getId());
+            stat.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public static void update(User user) {
+        Connection conn = null;
+        PreparedStatement stat = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            String sql = "update user set name=? ,password=? where id=?";
+            stat = conn.prepareStatement(sql);
+            stat.setObject(1, user.getName());
+            stat.setObject(2, user.getPassword());
+            stat.setObject(3, user.getId());
+            stat.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void save(User user) {
+        Connection conn = null;
+        PreparedStatement stat = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            String sql = "insert into user (id,name,password) values(null,?,?)";
+            stat = conn.prepareStatement(sql);
+            stat.setObject(1, user.getName());
+            stat.setObject(2, user.getPassword());
+            stat.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static List<User> select() {
         List<User> list = new ArrayList<>();
         Connection conn = null;
-        Statement stat = null;
+        PreparedStatement stat = null;
         try {
             //注册数据库驱动
             Class.forName(DRIVER);
             //获取连接
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            //实例化statement对象
-            stat = conn.createStatement();
             String sql = "select * from user";
-            ResultSet rs = stat.executeQuery(sql);
+            //实例化PreparedStatement对象
+            stat = conn.prepareStatement(sql);
+            ResultSet rs = stat.executeQuery();
             while (rs.next()) {
                 User user = new User();
                 Integer id = rs.getInt("id");
